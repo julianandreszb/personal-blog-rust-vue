de2be6b7e63b81687d3a6de7f76cd9ed8ddf45d9
Julian Zapata <julianandreszb@gmail.com>
2025-01-28 22:42:37 -0500

    Set up the migration and entity directory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use migration::{Migrator, MigratorTrait};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let database_url = std::env::var("DATABASE_URL")?;
+    
+    let connection = sea_orm::Database::connect(database_url).await?;
+    Migrator::up(&connection, None).await?;
+    Ok(())
 }
