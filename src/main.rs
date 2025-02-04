23a7bc4d3cd1d092e4ae67a4d55045e779a71e94
Julian Zapata <julianandreszb@gmail.com>
2025-02-04 06:15:57 -0500

    Implement get post by ID endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use axum::{extract::State, routing::get, Json, Router};
+use axum::extract::Path;
 use entity::post;
 use sea_orm::{ConnectOptions, DatabaseConnection, EntityTrait};
 
@@ -7,15 +8,15 @@ async fn main() {
 
     let database_url = std::env::var("DATABASE_URL")
         .expect(".cargo/config.toml `DATABASE_URL` variable must be set");
-
     let opt = ConnectOptions::new(database_url);
     let connection = sea_orm::Database::connect(opt).await.expect("Could not connect to database");
 
     let state = AppState { conn: connection };    
     
     let app = Router::new()
-        .route("/", get(|| async { "Hello, World!" }))
+        .route("/", get(|| async { "Hello, World! \nMy personal blog using Rust for backend!" }))
         .route("/posts", get(get_posts))
+        .route("/posts/{id}", get(get_post_by_id))
         .with_state(state);
 
     // run our app with hyper, listening globally on port 3000
@@ -26,6 +27,14 @@ async fn main() {
 async fn get_posts(state: State<AppState>) -> Result<Json<Vec<post::Model>>, (axum::http::StatusCode, String)> {
     match post::Entity::find().all(&state.conn).await {
         Ok(posts) => Ok(Json(posts)),
+        Err(err) => Err((axum::http::StatusCode::INTERNAL_SERVER_ERROR, err.to_string())),
+    }
+}
+
+async fn get_post_by_id(state: State<AppState>, Path(id): Path<i32>) -> Result<Json<post::Model>, (axum::http::StatusCode, String)> {
+    match post::Entity::find_by_id(id).one(&state.conn).await {
+        Ok(Some(post)) => Ok(Json(post)),
+        Ok(None) => Err((axum::http::StatusCode::NOT_FOUND, "Post not found".to_string())),
         Err(err) => Err((axum::http::StatusCode::INTERNAL_SERVER_ERROR, err.to_string())),
     }
 }
