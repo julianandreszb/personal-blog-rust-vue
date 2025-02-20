b318d7bd3f9c141578b37708e00e808580b6f0ba
Julian Zapata <julianandreszb@gmail.com>
2025-02-19 23:57:45 -0500

    Add DarkModeToggle component and update index.html for color scheme support

---
+++ frontend/src/components/UI/DarkModeToggle.vue
@@ -0,0 +1,104 @@
+<script setup>
+import { onMounted, onUnmounted } from 'vue'
+
+const useDark = window.matchMedia('(prefers-color-scheme: dark)')
+const toggleDarkMode = (evt) => {
+  if (evt.matches) {
+    document.documentElement.classList.add('dark-mode')
+  } else {
+    document.documentElement.classList.remove('dark-mode')
+  }
+  localStorage.setItem('dark-mode', useDark.matches)
+}
+
+onMounted(() => {
+  if (!localStorage.getItem('dark-mode')) {
+    localStorage.setItem('dark-mode', useDark.matches)
+  }
+
+  useDark.addEventListener('change', toggleDarkMode)
+  toggleDarkMode(useDark)
+})
+
+onUnmounted(() => {
+  useDark.removeEventListener('change', toggleDarkMode)
+})
+</script>
+
+<template>
+  <div class="dark-mode-toggle">
+    <label class="switch">
+      <input type="checkbox" />
+      <span class="slider">
+        <svg
+          class="slider-icon"
+          viewBox="0 0 24 24"
+          fill="none"
+          height="20"
+          stroke="#000"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          width="20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z" />
+        </svg>
+      </span>
+    </label>
+  </div>
+</template>
+
+<style scoped>
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
+
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: lightblue;
+  border-radius: 34px;
+  transition: 0.6s;
+}
+
+.slider:before {
+  position: absolute;
+  content: '';
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  border-radius: 50%;
+  transition: 0.4s;
+}
+
+input:checked + .slider {
+  background-color: #363f72;
+}
+
+input:checked + .slider:before {
+  transform: translateX(26px);
+}
+
+.slider-icon {
+  transform: rotate(250deg);
+  position: absolute;
+  right: 6px;
+  top: 7px;
+}
+</style>
