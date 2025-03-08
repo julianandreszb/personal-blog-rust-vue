60065c4464fa70468bfcd3b081b3eea580790084
Julian Zapata <julianandreszb@gmail.com>
2025-03-08 01:23:58 -0500

    Refactor router to organize post routes under a parent route and update component imports

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -8,22 +8,20 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
-    },
-    {
-      path: '/post/:slug',
-      name: 'post',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/TestView.vue'),
-    },
-    {
-      path: '/category/:slug',
-      name: 'category',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/TestView.vue'),
+      children: [
+        {
+          path: '',
+          name: 'posts',
+          default: true,
+          component: () => import('../views/PostsView.vue'),
+        },
+        {
+          path: '/:slug',
+          name: 'post',
+          component: () => import('../views/PostView.vue'),
+          props: true
+        },
+      ]
     },
   ],
 })
