4488c42f9e8926a67d1c7578816002b35d60e514
Julian Zapata <julianandreszb@gmail.com>
2025-03-18 21:38:12 -0500

    Refactor post model to remove content field, add content_md_id, and update unique constraints with comments

--- backend/src/bin/seed-data.rs
+++ backend/src/bin/seed-data.rs
@@ -1,5 +1,5 @@
 use chrono::Utc;
-use entity::sea_orm_active_enums::{ContentType, Language, Status};
+use entity::sea_orm_active_enums::{Language, Status};
 use entity::{category, post_category, post, post_tag, tag};
 use fake::Fake;
 use fake::faker::lorem::en::Paragraph;
@@ -73,13 +73,6 @@ async fn create_post(transaction: &sea_orm::DatabaseTransaction) -> Result<(), s
 
     // Generate a random post content
     let excerpt: String = Paragraph(1..2).fake_with_rng(&mut rng);
-    let content: String = Paragraph(3..6).fake_with_rng(&mut rng);
-
-    let content_type = match *["html", "markdown"].choose(&mut rng).unwrap() {
-        "html" => Some(ContentType::Html),
-        "markdown" => Some(ContentType::Markdown),
-        _ => None,
-    };
 
     let language = match *["es", "en"].choose(&mut rng).unwrap() {
         "es" => Some(Language::En),
@@ -100,8 +93,7 @@ async fn create_post(transaction: &sea_orm::DatabaseTransaction) -> Result<(), s
         title: Set(title),
         slug: Set(slug),
         excerpt: Set(excerpt),
-        content: Set(content),
-        content_type: Set(content_type),
+        content_md_id: Set("/".to_string()),
         language: Set(language),
         featured_image: Default::default(),
         status: Set(status),
@@ -111,55 +103,6 @@ async fn create_post(transaction: &sea_orm::DatabaseTransaction) -> Result<(), s
 
     Ok(())
 }
-
-// async fn create_post(transaction: &sea_orm::DatabaseTransaction) -> Result<(), sea_orm::error::DbErr> {
-//     //let mut rng = StdRng::from_entropy();
-//     let mut rng = StdRng::from_seed([0u8; 32]);
-//     
-//     // Generate a random post title
-//     let title: String = Word().fake_with_rng(&mut rng);
-//     let slug = title.to_lowercase().replace(" ", "-");
-//     
-//     // Generate a random post content
-//     let excerpt: String = Paragraph(1..2).fake_with_rng(&mut rng);
-//     let content: String = Paragraph(3..6).fake_with_rng(&mut rng);
-// 
-//     let content_type = match *["html", "markdown"].choose(&mut rng).unwrap() {
-//         "html" => Some(ContentType::Html),
-//         "markdown" => Some(ContentType::Markdown),
-//         _ => None,
-//     };
-// 
-//     let language = match *["es", "en"].choose(&mut rng).unwrap() {
-//         "es" => Some(Language::En),
-//         "en" => Some(Language::Es),
-//         _ => None,
-//     };
-// 
-//     let status = match *["draft", "published"].choose(&mut rng).unwrap() {
-//         "draft" => Some(Status::Draft),
-//         "published" => Some(Status::Published),
-//         _ => None,
-//     };
-//     
-//     let created_at = Option::from(Utc::now().naive_utc());
-// 
-//     post::ActiveModel {
-//         id: Default::default(),
-//         title: Set(title),
-//         slug: Set(slug),
-//         excerpt: Set(excerpt),
-//         content: Set(content),
-//         content_type: Set(content_type),
-//         language: Set(language),
-//         featured_image: Default::default(),
-//         status: Set(status),
-//         created_at: Set(created_at),
-//         updated_at: Default::default(),
-//     }.insert(transaction).await?;
-// 
-//     Ok(())
-// }
 
 async fn create_post_tag(transaction: &sea_orm::DatabaseTransaction) -> Result<(), sea_orm::error::DbErr> {
     
